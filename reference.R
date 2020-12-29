d7725c56de48a067b7e5007dc1d493588459783c
Michael Jones <51765945+jonesm30@users.noreply.github.com>
2020-12-29 10:59:20 -0800

    Delete reference.R

--- reference.R
+++
@@ -1,3 +0,0 @@
-rm(list=ls());                         # clear Console Window
-options(show.error.locations = TRUE);  # show line numbers on error
-library(package=ggplot2)              # include all GGPlot2 functions
